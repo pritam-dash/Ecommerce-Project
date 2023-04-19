2df443267bad95074c6fa8ea2a0395846be28b39
pritam-dash <pritamdash84@gmail.com>
2023-04-19 17:59:12 +0530

    ecommerce-app

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from './Header';
+import Footer from './Footer';
+import { Helmet } from "react-helmet";
+import { Toaster } from 'react-hot-toast';
+
+const Layout = ({ children,title,decription,keywords,author }) => {
+  return (
+    <div>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="description" content={decription} />
+                <meta name="keywords" content={keywords} />
+                <meta name="author" content={author} />
+                <title>{title}</title>
+            </Helmet>
+        <Header/>
+        <main style={{minHeight:"70vh"}}>{children}</main>
+        <Toaster />
+        <Footer/>
+    </div>
+  );
+};
+
+Layout.defaultProps = {
+  title: 'Ecommerce app - shop now',
+  description: 'mern stack project',
+  keywords: 'mern, react, node, mongodb',
+  author: 'Pritam',
+};
+
+export default Layout;
